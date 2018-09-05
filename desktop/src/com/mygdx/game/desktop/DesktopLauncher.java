c3d301ae0cf050f6c70a02f730d8f01a8f5e5cfb
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-05 14:36:16 -0400

    Initial commit of LibGDX base files

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.CSC361_F18_G5;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CSC361_F18_G5(), config);
+	}
+}
