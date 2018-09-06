58e5bd72e97e008ca45c7ec9044d67744e916a47
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-06 10:46:02 -0400

    Complete p131 code

--- core/src/com/mygdx/game/CameraHelper.java
+++ core/src/com/mygdx/game/CameraHelper.java
@@ -13,5 +13,39 @@ public class CameraHelper {
 	
 	private Vector2 position;
 	private float zoom;
+	private Sprite target;
+	
+	public CameraHelper () {
+		position = new Vector2();
+		zoom = 1.0f;
+	}
+	
+	public void update (float deltaTime) {
+		if (!hasTarget()) return;
+		
+		position.x = target.getX() + target.getOriginX();
+		position.y = target.getY() + target.getOriginY();
+	}
+	
+	public void setPosition (float x, float y) {
+		this.position.set(x, y);
+	}
+	
+	public Vector2 getPosition () { return position; }
+	
+	public float getZoom () { return zoom; }
+	
+	public void setTarget (Sprite target) { this.target = target; }
+	public Sprite getTarget () { return target; }
+	public boolean hasTarget () {return target != null; }
+	public boolean hasTarget (Sprite target) {
+		return hasTarget() && this.target.equals(target);
+	}
 
+	public void applyTo (OrthographicCamera camera) {
+		camera.position.x = position.x;
+		camera.position.y = position.y;
+		camera.zoom = zoom;
+		camera.update();
+	}
 }
