f3e5d7da563d38813ffb997ced31af69f5157507
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-06 10:09:30 -0400

    Complete p123 code

--- core/src/com/mygdx/game/WorldController.java
+++ core/src/com/mygdx/game/WorldController.java
@@ -13,21 +13,21 @@ public class WorldController {
 	public Sprite[] testSprites;
 	public int selectedSprite;
 	
-	public WorldController() {
+	public WorldController () {
 		init();
 	}
 	
-	private void init() {
+	private void init () {
 		initTestObjects();
 	}
 	
-	private void initTestObjects() {
+	private void initTestObjects () {
 		// Create new array for 5 sprites
 		testSprites = new Sprite[5];
 		// Create empty POT-sized Pixmap with 8 bit RGBA pixel data
 		int width = 32;
 		int height  = 32;
-		Pixmap pixmap = createProceduralPixmap(width, height);
+		Pixmap pixmap = createProceduralPixmap (width, height);
 		// Create a new texture from pixmap data
 		Texture texture = new Texture(pixmap);
 		// Create new sprites using the just created texture
@@ -48,10 +48,33 @@ public class WorldController {
 		selectedSprite = 0;
 	}
 	
-	private Pixap createProceduralPixmap (int width, int height) {
+	private Pixmap createProceduralPixmap (int width, int height) {
 		Pixmap pixmap = new Pixmap(width, height, Format.RGBA8888);
 		// Fill square with red color at 50% opacity
+		pixmap.setColor(1, 0, 0, 0.5f);
+		pixmap.fill();
+		// Draw a yellow-colored X shape on square
+		pixmap.setColor(1, 1, 0, 1);
+		pixmap.drawLine(0,  0,  width,  height);
+		pixmap.drawLine(width, 0, 0, height);
+		// Draw a cyan-colored border around square
+		pixmap.setColor(0, 1, 1, 1);
+		pixmap.drawRectangle(0,  0,  width,  height);
+		return pixmap;
+	}
 	
+	public void update (float deltaTime) {
+		updateTestObjects(deltaTime);
+	}
 	
-	public void update (float deltaTime) { }
+	private void updateTestObjects (float deltaTime) {
+		// Get current rotation from selected sprite
+		float rotation = testSprites[selectedSprite].getRotation();
+		// Rotate sprite by 90 degrees per second
+		rotation += 90 * deltaTime;
+		// Wrap around at 360 degrees
+		rotation %= 360;
+		// Set new rotation value to selected sprite
+		testSprites[selectedSprite].setRotation(rotation);
+	}
 }
