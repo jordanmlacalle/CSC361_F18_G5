3d79e1b146ec1039218ddafdc63b2cff1523568d
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-10 12:50:23 -0400

    Change to run Canyon Bunny instead of libGDX demo

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.CSC361_F18_G5;
+//import com.mygdx.game.CSC361_F18_G5;
+import com.mygdx.game.*;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CSC361_F18_G5(), config);
+		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
 }
