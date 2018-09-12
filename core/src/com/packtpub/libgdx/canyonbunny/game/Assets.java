471eb64a7b13f9c11d259ad75ff477abf01f9a41
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-12 13:19:28 -0400

    Complete p156 code

--- core/src/com/packtpub/libgdx/canyonbunny/game/Assets.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/Assets.java
@@ -17,6 +17,12 @@ public class Assets implements Disposable, AssetErrorListener {
     
     public static final Assets instance = new Assets();
     
+    public AssetBunny bunny;
+    public AssetRock rock;
+    public AssetGoldCoin goldCoin;
+    public AssetFeather feather;
+    public AssetLevelDecoration levelDecoration;
+    
     private AssetManager assetManager;
     
     //singleton: prevent instantiation from other classes
@@ -30,9 +36,25 @@ public class Assets implements Disposable, AssetErrorListener {
         assetManager.load(Constants.TEXTURE_ATLAS_OBJECTS, TextureAtlas.class);
         // start loading assets and wait until finished
         assetManager.finishLoading();
+        
         Gdx.app.debug(TAG, "# of assets loaded: " + assetManager.getAssetNames().size);
-        for (String a : assetManager.getAssetNames())
+        for (String a : assetManager.getAssetNames()) {
             Gdx.app.debug(TAG, "asset: " + a);
+        }
+        
+        TextureAtlas atlas = asseManager.get(Constants.TEXTURE_ATLAS_OBJECTS);
+        
+        // enable texture filtering for pixel smoothing
+        for (Texture t : atlas.getTextures()) {
+            t.setFilter(TextureFilter.Linear,  TextureFilter.Linear);
+        }
+        
+        // create game resource objects
+        bunny = new AssetBunny(atlas);
+        rock = new AssetRock(atlas);
+        goldCoin = new AssetGoldCoin(atlas);
+        feather = new AssetFeather(atlas);
+        levelDecoration = new AssetLevelDecoration(atlas);
     }
     
     @Override
