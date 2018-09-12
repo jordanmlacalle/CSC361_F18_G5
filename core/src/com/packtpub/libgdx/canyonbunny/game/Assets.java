10ae9be79739705884893ce56f44ce46f5611f01
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-12 09:22:23 -0400

    Complete p149 code

---
+++ core/src/com/packtpub/libgdx/canyonbunny/game/Assets.java
@@ -0,0 +1,14 @@
+package com.packtpub.libgdx.canyonbunny.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Disposable;
+import com.packtpub.libgdx.canyonbunny.util.Constants;
+
+public class Assets implements Disposable, AssetErrorListener {
+    
+
+}
