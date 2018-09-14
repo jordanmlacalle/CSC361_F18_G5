3f20b25784406b7e2c18a330222a06c78e397a47
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-14 14:46:12 -0400

    Resolve texture packing errors
    Modified texture atlas to have .atlas extension

--- core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
+++ core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	  public static final float VIEWPORT_HEIGHT = 5.0f; 
 	  
 	  // Location of description file for texture atlas 
-	  public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack";
+	  public static final String TEXTURE_ATLAS_OBJECTS = "../CanyonBunny-desktop/assets/images/canyonbunny.atlas";
 	  
 	  // Location of image file for level 01
       public static final String LEVEL_01 = "levels/level-01.png";
