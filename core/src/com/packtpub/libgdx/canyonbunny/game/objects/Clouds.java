ea8e93c53578e80b34df6379a72a744cdb5844ea
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-16 16:47:10 -0400

    Complete p176 code

--- core/src/com/packtpub/libgdx/canyonbunny/game/objects/Clouds.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/Clouds.java
@@ -2,6 +2,10 @@ package com.packtpub.libgdx.canyonbunny.game.objects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.packtpub.libgdx.canyonbunny.game.Assets;
 
 public class Clouds extends AbstractGameObject {
     
@@ -46,8 +50,52 @@ public class Clouds extends AbstractGameObject {
         init();
     }
     
-    private void init()
-    {
+    /**
+     * Initialize texture region array and spawn clouds.
+     */
+    private void init() {
+        dimension.set(3.0f, 1.5f);
+        regClouds = new Array<TextureRegion>();
+        regClouds.add(Assets.instance.levelDecoration.cloud01);
+        regClouds.add(Assets.instance.levelDecoration.cloud02);
+        regClouds.add(Assets.instance.levelDecoration.cloud03);
         
+        int distFac = 5;
+        int numClouds = (int)(length / distFac);
+        clouds = new Array<Cloud>(2 * numClouds);
+        for (int i = 0; i < numClouds; i++) {
+            Cloud cloud = spawnCloud();
+            cloud.position.x = i * distFac;
+            clouds.add(cloud);
+        }
     }
-}
+    
+    /**
+     * Creates and initializes a new cloud object
+     * @return Returns a new cloud object
+     */
+    private Cloud spawnCloud() {
+        Cloud cloud = new Cloud();
+        cloud.dimension.set(dimension);
+        // Select random cloud image
+        cloud.setRegion(reg.Clouds.random());
+        //position
+        Vector2 pos = new Vector2();
+        pos.x = length + 10; // position after end of level
+        pos.y += 1.75; //base position
+        pos.y += MathUtils.random(0.0f, 0.2f) + (MathUtils.randomBoolean() ? 1 : -1); // random additional position
+        cloud.position.set(pos);
+        return cloud;
+    }
+    
+    /**
+     * Implementation of the inherited render method (from AbstractGameObject).
+     * Renders all clouds contained in the clouds array.
+     * @param batch
+     */
+    @Override
+    public void render (SpriteBatch batch) {
+        for (Cloud cloud : clouds)
+            cloud.render(batch);
+    }
+ }
