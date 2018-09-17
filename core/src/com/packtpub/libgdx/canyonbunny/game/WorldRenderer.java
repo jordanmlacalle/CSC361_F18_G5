39bcc8f6c802ed2b75d1314125e9fcafd23c15c8
Sam Selkregg <sjselkregg@gmail.com>
2018-09-17 09:14:43 -0400

    Updated World Renderer p189-191
    Updated world renderer code

--- core/src/com/packtpub/libgdx/canyonbunny/game/WorldRenderer.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/WorldRenderer.java
@@ -1,5 +1,6 @@
 package com.packtpub.libgdx.canyonbunny.game;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
@@ -16,11 +17,18 @@ public class WorldRenderer implements Disposable {
 	  init();
   }
   
+  private OrthographicCamera cameraGUI;
+  
   private void init () { 
 	  batch = new SpriteBatch();
 	  camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
 	  camera.position.set(0, 0, 0);
 	  camera.update();
+	  
+	  cameraGUI = new OrthographicCamera(Constants.VIEWPORT_GUI_WIDTH,Constants.VIEWPORT_GUI_HEIGHT);
+	  cameraGUI.position.set(0, 0, 0);
+	  cameraGUI.setToOrtho(true); // flip y-axis
+	  cameraGUI.update();
   }
 
   public void render () {
@@ -38,9 +46,38 @@ public class WorldRenderer implements Disposable {
   public void resize (int width, int height) { 
 	  camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) * width;
 	  camera.update();
+	  
+	  cameraGUI.viewportHeight = Constants.VIEWPORT_GUI_HEIGHT;
+      cameraGUI.viewportWidth = (Constants.VIEWPORT_GUI_HEIGHT / (float)height) * (float)width;
+      cameraGUI.position.set(cameraGUI.viewportWidth / 2, cameraGUI.viewportHeight / 2, 0);
+      cameraGUI.update();
   }
 
   @Override public void dispose () {
 	  batch.dispose();
+  }
+  
+  private void renderGuiScore (SpriteBatch batch) 
+  {
+      float x = -15;
+      float y = -15;
+      batch.draw(Assets.instance.goldCoin.goldCoin,
+          x, y, 50, 50, 100, 100, 0.35f, -0.35f, 0);
+      Assets.instance.fonts.defaultBig.draw(batch,
+          "" + worldController.score,
+          x + 75, y + 37);
+    }
+  
+  private void renderGuiExtraLive (SpriteBatch batch) {
+      float x = cameraGUI.viewportWidth - 50 - Constants.LIVES_START * 50;
+      float y = -15;
+      for (int i = 0; i < Constants.LIVES_START; i++) 
+      {
+        if (worldController.lives <= i)
+          batch.setColor(0.5f, 0.5f, 0.5f, 0.5f);
+        batch.draw(Assets.instance.bunny.head,
+            x + i * 50, y, 50, 50, 120, 100, 0.35f, -0.35f, 0);
+        batch.setColor(1, 1, 1, 1);
+      } 
   }
 }
