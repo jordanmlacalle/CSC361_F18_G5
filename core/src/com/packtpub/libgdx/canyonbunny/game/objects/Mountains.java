dd86f81f2bb84da8f68658e6f36b513530e88ec1
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-17 10:32:21 -0400

    Resolve Mountains.java errors

--- core/src/com/packtpub/libgdx/canyonbunny/game/objects/Mountains.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/Mountains.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.packtpub.libgdx.canyonbunny.game.Assets;
 
-public class Mountains {
+public class Mountains extends AbstractGameObject {
     /**
      * regMountainLeft - texture region for the left side of the Mountain...
      * regMountainRight - texture region for the right side of the Mountain...
@@ -42,7 +42,7 @@ public class Mountains {
      * @param tintColor the tint of the mountain
      */
     private void drawMountain (SpriteBatch batch, float offsetX, float offsetY, float tintColor) {
-        TextureReg reg = null;
+        TextureRegion reg = null;
         batch.setColor(tintColor, tintColor, tintColor, 1);
         float xRel = dimension.x * offsetX;
         float yRel = dimension.y * offsetY;
@@ -54,12 +54,12 @@ public class Mountains {
         for (int i = 0; i < mountainLength; i++) {
             // Mountain left
             reg = regMountainLeft;
-            batch.draw(reg.getTexture(), origin.x + xRel, origin.y + yRel, origin.x, origin.y, dimension.x, diension.y, scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(), reg.getRegionHeight(), false, false);
+            batch.draw(reg.getTexture(), origin.x + xRel, origin.y + yRel, origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(), reg.getRegionHeight(), false, false);
             xRel += dimension.x;
             
             // Mountain right
             reg = regMountainRight;
-            batch.draw(reg.getTexture(), oigin.x + xRel, position.y + origin.y + yRel, origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y, rotation, reg.getRegionX()   reg.getRegionY(), reg.getRegionWidth(), reg.getRegionHeight(), false, false);
+            batch.draw(reg.getTexture(), origin.x + xRel, position.y + origin.y + yRel, origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(), reg.getRegionHeight(), false, false);
             xRel += dimension.x;
         }
         // Reset color to white
