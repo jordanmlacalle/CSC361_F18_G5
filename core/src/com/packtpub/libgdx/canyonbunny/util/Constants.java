afe79881748332b6c6979d54159dbba9126c19fb
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-09-17 13:09:45 -0400

    Change assets path
    Changed assets paths in DesktopLauncher and Constants

--- core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
+++ core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
 	  public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
 	  
 	  // Location of description file for texture atlas 
-	  public static final String TEXTURE_ATLAS_OBJECTS = "../CanyonBunny-desktop/assets/images/canyonbunny.atlas";
+	  public static final String TEXTURE_ATLAS_OBJECTS = "../core/assets/images/canyonbunny.atlas";
 	  
 	  // Location of image file for level 01
       public static final String LEVEL_01 = "assets/levels/level-01.png";
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -18,7 +18,7 @@ public class DesktopLauncher {
             settings.maxHeight = 1024;
             settings.duplicatePadding = false;
             settings.debug = drawDebugOutline;
-            TexturePacker.process(settings, "assets-raw/images", "../CanyonBunny-desktop/assets/images","canyonbunny");
+            TexturePacker.process(settings, "assets-raw/images", "../core/assets/images","canyonbunny");
 		}
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new CanyonBunnyMain(), config);
